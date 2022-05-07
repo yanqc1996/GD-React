82a4ddb0946b1c44fea37ec8a38b9a2be4493a7c
a_mao <a_mao@hunliji.com>
2022-05-07 09:56:51 +0800

    demo init

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  semi: false, // 行尾无分号
+  // 对象的 key 仅在必要时用引号
+  quoteProps: 'as-needed',
+  // 使用默认的折行标准
+  proseWrap: 'preserve',
+  // 根据显示样式决定 html 要不要折行
+  htmlWhitespaceSensitivity: 'css',
+  // vue 文件中的 script 和 style 内不用缩进
+  vueIndentScriptAndStyle: false,
+  // jsx 不使用单引号，而使用双引号
+  jsxSingleQuote: false,
+  // 末尾需要有逗号
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  // 换行符使用 lf
+  endOfLine: 'lf',
+  // 格式化嵌入的内容
+  embeddedLanguageFormatting: 'auto',
+}
